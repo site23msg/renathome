0e32e9d5bbd23b19072bae2134bb09b0474a9429
Galaczi Arnold <sitebuild@23msg.com>
2022-10-27 10:14:26 +0200

    welcome text loop

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const homeAnchor = document.getElementById("homeanchor");
 const aboutAnchor = document.getElementById("aboutanchor");
 const galleryAnchor = document.getElementById("galleryanchor");
+const bookingAnchor = document.getElementById("bookinganchor");
 const contactAnchor = document.getElementById("contactanchor");
 
 const homeSection = document.getElementById("home")
 const aboutSection = document.getElementById("about")
 const gallerySection = document.getElementById("gallery")
+const bookingSection = document.getElementById("booking")
 const contactSection = document.getElementById("contact")
+
+const welcomeText = document.querySelector('#welcome-text')
+
+const welcomeTexts = ['Welcome', 'Wilkommen', 'Üdvözöllek']
+
+let counter = 0;
+
+function display () {
+    if(counter < welcomeTexts.length) {
+        welcomeText.innerHTML = welcomeTexts[counter]
+        counter++;
+    } else {
+        counter = 0;
+    }
+}
+
+setInterval(display,2000)
 
 homeAnchor.addEventListener('click', (e) => {
     e.preventDefault();
@@ -39,3 +58,30 @@ contactAnchor.addEventListener('click', (e) => {
         behavior: 'smooth'
     });
 })
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+const body = document.querySelector("body");
+
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    body.classList.add("overflowyhidden");
+  };
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+    body.classList.remove("overflowyhidden")
+};
+
+closeModalBtn.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
